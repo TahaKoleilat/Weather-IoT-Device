b00c4f979e726c5b8d71f02b42d955368310e638
Taha Koleilat <78985685+TahaKoleilat@users.noreply.github.com>
2022-12-07 18:09:16 +0200

    Update esp_mqtt_client.c

--- IoT Weather Client/main/esp_mqtt_client.c
+++ IoT Weather Client/main/esp_mqtt_client.c
@@ -148,7 +148,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 void mqtt_app_start(void)
 {
     esp_mqtt_client_config_t mqtt_cfg = {
-        .uri = "mqtt://192.168.1.106:1883",  //Change to mqtt://(device's IPv4 address):1883
+        .uri = "mqtt://local_ip_address:1883",  //Change to mqtt://(device's IPv4 address):1883
     };
     esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client);
